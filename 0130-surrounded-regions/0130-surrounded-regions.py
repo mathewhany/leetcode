580831c05c428f3cb2d0b542f0ef2b003e9a18da
Mathew Hany <mathew.hanybb@gmail.com>
2023-04-27 09:11:55 +0200

    Time: 159 ms (20.23%), Space: 18.2 MB (17.9%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -1,22 +1,20 @@
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
         ROWS, COLS = len(board), len(board[0])
-
+        DIRS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         def dfs(i, j):
             if not (0 <= i < ROWS and 0 <= j < COLS) or board[i][j] != 'O':
                 return
             
             board[i][j] = '-'
-            dfs(i + 1, j)
-            dfs(i - 1, j)
-            dfs(i, j + 1)
-            dfs(i, j - 1)
+            for di, dj in DIRS:
+                dfs(i + di, j + dj)
                     
         for row in range(ROWS):
             dfs(row, 0)
             dfs(row, COLS - 1)
 
-        for col in range(COLS):
+        for col in range(1, COLS - 1):
             dfs(0, col)
             dfs(ROWS - 1, col)
 
