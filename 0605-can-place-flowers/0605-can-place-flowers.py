577afb1425ab1915bbe11392af47e8ed20b10176
Mathew Hany <mathew.hanybb@gmail.com>
2023-06-16 20:56:39 +0300

    Time: 235 ms (5.02%), Space: 36 MB (24.07%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -11,7 +11,7 @@ class Solution:
             next = flowerbed[i + 1] if i + 1 < len(flowerbed) else 0
             current = flowerbed[i]
 
-            ans = memo[(i, prev)] = max(1 + dp(i + 1, 1), dp(i + 1, 0)) if prev + next + current == 0 else dp(i + 1, current)
+            ans = memo[(i, prev)] = max(1 + dp(i + 2, next), dp(i + 1, 0)) if prev + next + current == 0 else dp(i + 1, current)
 
             return ans
         return dp(0, 0) >= n
