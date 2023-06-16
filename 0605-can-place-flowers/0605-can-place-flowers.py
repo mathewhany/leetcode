09828fa4bbc733794a142f83efa4bac2f694076a
Mathew Hany <mathew.hanybb@gmail.com>
2023-06-16 20:53:34 +0300

    Time: 253 ms (5.02%), Space: 39.3 MB (24.07%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        memo = {}
+        def dp(i, prev):
+            if (i, prev) in memo:
+                return memo[(i, prev)]
+
+            if i >= len(flowerbed):
+                return 0
+            
+            next = flowerbed[i + 1] if i + 1 < len(flowerbed) else 0
+            current = flowerbed[i]
+
+            ans = memo[(i, prev)] = max(1 + dp(i + 1, 1), dp(i + 1, 0)) if prev + next + current == 0 else dp(i + 1, current)
+
+            return ans
+        return dp(0, 0) >= n
