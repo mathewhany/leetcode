03d9370507e7dd1e292c25160bbb5156ec8cffbf
Mathew Hany <mathew.hanybb@gmail.com>
2023-06-24 22:00:25 +0300

    Time: 45 ms (77.43%), Space: 16.4 MB (33.07%) - LeetHub

--- 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.py
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.py
@@ -1,15 +1,22 @@
 class Codec:
+    alphabet = '0123456789' + string.ascii_letters
 
     def __init__(self):
         self.decodeMap = {}
-        self.base = "http://tinyurl.com/"
-        self.current = 0
+        self.baseUrl = "http://tinyurl.com/"
+        self.nextId = 0
 
     def encode(self, longUrl: str) -> str:
-        url = self.base + str(self.current)
-        self.current += 1
-        self.decodeMap[url] = longUrl
-        return url
+        base = len(self.alphabet)
+        encoded = ''
+        currentId = self.nextId
+        self.nextId += 1
+        while currentId > 0:
+            encoded = alphabet[current % base] + encoded
+            currentId = currentId // base
+        shortUrl = self.baseUrl + (encoded if len(encoded) > 0 else '0')
+        self.decodeMap[shortUrl] = longUrl
+        return shortUrl
 
     def decode(self, shortUrl: str) -> str:
         return self.decodeMap[shortUrl]
