3b58e3b5fffd1325b1c7f5ecb757fcd940179499
Mathew Hany <mathew.hanybb@gmail.com>
2023-06-27 10:37:07 +0300

    Time: 1723 ms (34.05%), Space: 73.4 MB (8.47%) - LeetHub

---
+++ 2129-number-of-pairs-of-interchangeable-rectangles/2129-number-of-pairs-of-interchangeable-rectangles.py
@@ -0,0 +1,14 @@
+class Solution:
+    def interchangeableRectangles(self, rectangles: List[List[int]]) -> int:
+        groups = collections.defaultdict(int)
+
+        for width, height in rectangles:
+            groups[width / height] += 1
+        
+        ans = 0
+        for group in groups:
+            n = groups[group]
+            if n > 1:
+                ans += n * (n - 1) / 2
+        
+        return int(ans)
