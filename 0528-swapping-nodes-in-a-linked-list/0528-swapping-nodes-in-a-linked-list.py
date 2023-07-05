3705e2da14f5b11d76b38f6d3447e72af2adf66c
Mathew Hany <mathew.hanybb@gmail.com>
2023-07-05 16:38:42 +0300

    Time: 1000 ms (43.69%), Space: 50.7 MB (54.34%) - LeetHub

--- 0528-swapping-nodes-in-a-linked-list/0528-swapping-nodes-in-a-linked-list.py
+++ 0528-swapping-nodes-in-a-linked-list/0528-swapping-nodes-in-a-linked-list.py
@@ -23,24 +23,8 @@ def getNode(head, i):
 class Solution:
     def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         n = count(head)
-        dummy = ListNode(0, head)
-        pre1, pre2 = getNode(dummy, k - 1), getNode(dummy, n - k)
-        node1, node2 = pre1.next, pre2.next
-        next1, next2 = node1.next, node2.next
+        node1, node2 = getNode(head, k - 1), getNode(head, n - k)
+        node1.val, node2.val = node2.val, node1.val
 
-        if node2 == next1:
-            pre1.next = node2
-            node2.next = node1
-            node1.next = next2
-        elif node1 == next2:
-            pre2.next = node1
-            node1.next = node2
-            node2.next = next1
-        else:
-            pre1.next = node2
-            node2.next = next1
-            pre2.next = node1
-            node1.next = next2
-        
-        return dummy.next
+        return head
         
