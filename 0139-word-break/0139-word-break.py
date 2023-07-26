657aa7a7b52d85e5a039bbe08a28512c9cabdbe1
Mathew Hany <mathew.hanybb@gmail.com>
2023-07-26 10:03:25 +0200

    Time: 68 ms (9.24%), Space: 16.5 MB (31.95%) - LeetHub

---
+++ 0139-word-break/0139-word-break.py
@@ -0,0 +1,18 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        wordSet = set(wordDict)
+        memo = {}
+
+        def dp(target):
+            if target in wordSet: return True
+            if target in memo: return memo[target]
+
+            for i in range(1, len(target)):
+                prefix, suffix = target[:i], target[i:]
+                if prefix in wordSet and (suffix in wordSet or dp(suffix)): 
+                    memo[target] = True
+                    return True
+            memo[target] = False
+            return False
+
+        return dp(s)
