aeded4fccad98da15759605682ee8eee13fd983f
Mathew Hany <mathew.hanybb@gmail.com>
2023-07-27 18:40:20 +0200

    Time: 94 ms (50.98%), Space: 16.5 MB (66.32%) - LeetHub

--- 1130-last-stone-weight-ii/1130-last-stone-weight-ii.py
+++ 1130-last-stone-weight-ii/1130-last-stone-weight-ii.py
@@ -1,22 +1,23 @@
 class Solution:
     def lastStoneWeightII(self, stones: List[int]) -> int:
         stonesSum = sum(stones)
-        target = stonesSum / 2
-        memo = {}
+        target = stonesSum // 2
+        dp = [[0 for _ in range(target + 1)] for _ in range(len(stones))]
 
-        def dp(i, total):
-            if (i, total) in memo: return memo[(i, total)]
+        for i in range(len(stones) - 1, -1, -1):
+            for total in range(target, -1, -1):
+                if i + 1 >= len(stones):
+                    dp[i][total] = max(
+                        total,
+                        total + stones[i] if total + stones[i] <= target else -inf
+                    )
+                else:
+                    dp[i][total] = max(
+                        dp[i + 1][total],
+                        dp[i + 1][total + stones[i]] if total + stones[i] <= target else -inf
+                    )
 
-            if total > target: return -inf
-            if i >= len(stones): return total
 
-            memo[(i, total)] = ans = max(
-                dp(i + 1, total), 
-                dp(i + 1, total + stones[i])
-            )
-
-            return ans
-        
-        S = dp(0, 0)
+        S = dp[0][0]
 
         return stonesSum - 2 * S
