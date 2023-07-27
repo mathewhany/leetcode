342cf8d1c0729c2f33be85b03b750749d727e1d3
Mathew Hany <mathew.hanybb@gmail.com>
2023-07-27 08:29:23 +0200

    Time: 55 ms (75.53%), Space: 16.5 MB (58.8%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,17 +1,17 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         m, n = len(obstacleGrid), len(obstacleGrid[0])
-        memo = {}
-        
-        def dp(i, j):
-            if (i, j) in memo: return memo[(i, j)]
+        dp = [[0 for _ in range(n)] for _ in range(m)]
 
-            if not (0 <= i < m and 0 <= j < n) or obstacleGrid[i][j] == 1: return 0
+        for j in range(n - 1, -1, -1):
+            for i in range(m - 1, -1, -1):
+                if obstacleGrid[i][j] == 1: 
+                    dp[i][j] = 0
+                elif (i, j) == (m - 1, n - 1):
+                    dp[i][j] = 1
+                else:
+                    bottom = dp[i + 1][j] if i + 1 < m else 0
+                    right = dp[i][j + 1] if j + 1 < n else 0
+                    dp[i][j] = bottom + right
 
-            if (i, j) == (m - 1, n - 1): return 1
-
-            memo[(i, j)] = ans = dp(i + 1, j) + dp(i, j + 1)
-
-            return ans
-        
-        return dp(0, 0)
+        return dp[0][0]
