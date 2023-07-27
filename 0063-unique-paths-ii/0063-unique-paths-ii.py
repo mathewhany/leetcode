14c1ab8599234893325fec49b39acba2fdd7b33e
Mathew Hany <mathew.hanybb@gmail.com>
2023-07-27 08:31:10 +0200

    Time: 55 ms (75.53%), Space: 16.5 MB (58.8%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -3,15 +3,15 @@ class Solution:
         m, n = len(obstacleGrid), len(obstacleGrid[0])
         dp = [[0 for _ in range(n)] for _ in range(m)]
 
-        for j in range(n - 1, -1, -1):
-            for i in range(m - 1, -1, -1):
+        for i in range(m):
+            for j in range(n):
                 if obstacleGrid[i][j] == 1: 
                     dp[i][j] = 0
-                elif (i, j) == (m - 1, n - 1):
+                elif (i, j) == (0, 0):
                     dp[i][j] = 1
                 else:
-                    bottom = dp[i + 1][j] if i + 1 < m else 0
-                    right = dp[i][j + 1] if j + 1 < n else 0
-                    dp[i][j] = bottom + right
+                    top = dp[i - 1][j] if i - 1 >= 0 else 0
+                    left = dp[i][j - 1] if j - 1 >= 0 else 0
+                    dp[i][j] = top + left
 
-        return dp[0][0]
+        return dp[m - 1][n - 1]
