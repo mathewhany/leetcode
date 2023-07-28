d293acba1ac34545a21e583fa59e45833b353b03
Mathew Hany <mathew.hanybb@gmail.com>
2023-07-28 09:49:23 +0200

    Time: 85 ms (99.93%), Space: 17.7 MB (90.7%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -4,14 +4,14 @@ class Solution:
 
         dp = [[0 for _ in range(n)] for _ in range(m)]        
         
-        for i in range(m - 1, -1, -1):
-            for j in range(n - 1, -1, -1):
-                if (i, j) == (m - 1, n - 1): 
+        for i in range(m):
+            for j in range(n):
+                if (i, j) == (0, 0): 
                     dp[i][j] = grid[i][j]
                 else:
                     dp[i][j] = grid[i][j] + min(
-                        dp[i + 1][j] if i + 1 < m else inf, 
-                        dp[i][j + 1] if j + 1 < n else inf
+                        dp[i - 1][j] if i - 1 >= 0 else inf, 
+                        dp[i][j - 1] if j - 1 >= 0 else inf
                     )
 
-        return dp[0][0]
+        return dp[-1][-1]
