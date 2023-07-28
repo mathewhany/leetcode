c14facf260ea3e175aba8aec68130d8e14a92df5
Mathew Hany <mathew.hanybb@gmail.com>
2023-07-28 09:41:02 +0200

    Time: 211 ms (50.14%), Space: 16.3 MB (73.11%) - LeetHub

--- 0909-stone-game/0909-stone-game.py
+++ 0909-stone-game/0909-stone-game.py
@@ -1,15 +1,15 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        dp = [[(0, 0) for _ in range(len(piles))] for _ in range(len(piles))]
+        dp = [(0, 0) for _ in range(len(piles))]
 
         for start in range(len(piles) - 1, -1, -1):
             for end in range(start, len(piles)):
-                enemyScore1, myScore1 = dp[start + 1][end] if start + 1 < len(piles) else (0, 0)
-                enemyScore2, myScore2 = dp[start][end - 1] if end - 1 >= start else (0, 0)
+                enemyScore1, myScore1 = dp[end] if start + 1 < len(piles) else (0, 0)
+                enemyScore2, myScore2 = dp[end - 1] if end - 1 >= start else (0, 0)
                 myScore1 += piles[start]
                 myScore2 += piles[end]
 
-                dp[start][end] = (myScore1, enemyScore1) if myScore1 > myScore2 else (myScore2, enemyScore2)
+                dp[end] = (myScore1, enemyScore1) if myScore1 > myScore2 else (myScore2, enemyScore2)
 
 
-        return dp[0][len(piles) - 1]
+        return dp[len(piles) - 1]
