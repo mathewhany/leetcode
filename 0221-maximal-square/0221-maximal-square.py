377d5133879b176ba8de9ea153930cef3c51c0ae
Mathew Hany <mathew.hanybb@gmail.com>
2023-07-28 10:26:41 +0200

    Time: 613 ms (99.7%), Space: 19.4 MB (32.8%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -11,14 +11,12 @@ class Solution:
                     left = dp[i][j - 1] if j - 1 >= 0 else 0
                     topleft = dp[i - 1][j - 1] if i - 1 >= 0 and j - 1 >= 0 else 0
 
-                    sideLen = min(
-                        sqrt(top),
-                        sqrt(left),
-                        sqrt(topleft)
+                    dp[i][j] = min(
+                        top,
+                        left,
+                        topleft
                     ) + 1
-
-                    dp[i][j] = int(sideLen ** 2)
                 
                 best = max(best, dp[i][j])
 
-        return best
+        return best * best
