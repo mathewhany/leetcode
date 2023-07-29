2b48077632f7e0ed92f3928cec8ce81a58b74628
Mathew Hany <mathew.hanybb@gmail.com>
2023-07-29 16:15:46 +0200

    Time: 1589 ms (77.78%), Space: 16.6 MB (98.1%) - LeetHub

--- 0911-profitable-schemes/0911-profitable-schemes.py
+++ 0911-profitable-schemes/0911-profitable-schemes.py
@@ -1,21 +1,18 @@
 class Solution:
     def profitableSchemes(self, n: int, minProfit: int, group: List[int], profit: List[int]) -> int:
         nCrimes = len(group)
-        maxProfit = sum(profit)
-        dp = defaultdict(int)
+        dp = [[0 for _ in range(minProfit + 1)] for _ in range(n + 1)]
 
         for i in range(nCrimes, -1, -1):
-            for available in range(n + 1):
-                for totalProfit in range(minProfit + 1, -1, -1):
+            for m in range(n, -1, -1):
+                for totalProfit in range(minProfit + 1):
                     if i >= nCrimes: 
-                        dp[(i, available, totalProfit)] = 1 if totalProfit >= minProfit else 0
+                        dp[m][totalProfit] = 1 if totalProfit >= minProfit else 0
                     else:
-                        dp[(i, available, totalProfit)] = dp[i + 1, available, totalProfit]
-
-                        if available >= group[i]:
-                            dp[(i, available, totalProfit)] += dp[(i + 1, available - group[i], min(minProfit, totalProfit + profit[i]))]
+                        if m >= group[i]:
+                            dp[m][totalProfit] += dp[m - group[i]][min(totalProfit + profit[i], minProfit)]
                         
-                        dp[(i, available, totalProfit)] %= 10 ** 9 + 7
+                        dp[m][totalProfit] %= 10 ** 9 + 7
 
 
-        return dp[(0, n, 0)]
+        return dp[n][0]
