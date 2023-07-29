4fe489981b79096a0fab02a8771ca774fe372b20
Mathew Hany <mathew.hanybb@gmail.com>
2023-07-29 09:21:37 +0200

    Time: 2108 ms (5.11%), Space: 17.3 MB (81.29%) - LeetHub

--- 1240-stone-game-ii/1240-stone-game-ii.py
+++ 1240-stone-game-ii/1240-stone-game-ii.py
@@ -1,29 +1,25 @@
 class Solution:
     def stoneGameII(self, piles: List[int]) -> int:
-        memo = {}
+        n = len(piles)
+        dp = [[(0, 0) for _ in range(n + 1)] for _ in range(n + 1)]
+        
+        for i in range(n - 1, -1, -1):
+            for m in range(1, n + 1):
+                currentSum = 0
+                bestScore = 0
+                worstEnemyScore = inf
 
-        def dp(i, m):
-            if (i, m) in memo: return memo[(i, m)]
+                for x in range(1, 2 * m + 1):
+                    if i + x - 1 >= len(piles): break 
+                    currentSum += piles[i + x - 1]
+                    enemyScore, myRestScore = dp[i + x][max(x, m)]
+                    myScore = currentSum + myRestScore
+                    if myScore > bestScore:
+                        bestScore = myScore
+                        worstEnemyScore = enemyScore
+                
+                dp[i][m] = (bestScore, worstEnemyScore)
 
-            if i >= len(piles): return (0, 0)
-
-            currentSum = 0
-            bestScore = 0
-            worstEnemyScore = inf
-
-            for x in range(1, 2 * m + 1):
-                if i + x - 1 >= len(piles): break 
-                currentSum += piles[i + x - 1]
-                enemyScore, myRestScore = dp(i + x, max(x, m))
-                myScore = currentSum + myRestScore
-                if myScore > bestScore:
-                    bestScore = myScore
-                    worstEnemyScore = enemyScore
-            
-            memo[(i, m)] = ans = (bestScore, worstEnemyScore)
-
-            return ans
-
-        myScore, _ = dp(0, 1)
+        myScore, _ = dp[0][1]
 
         return myScore
