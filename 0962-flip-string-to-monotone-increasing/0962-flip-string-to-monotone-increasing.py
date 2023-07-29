f623b2309349b227540dc67b89d2b51f026f357f
Mathew Hany <mathew.hanybb@gmail.com>
2023-07-29 09:38:32 +0200

    Time: 449 ms (21.33%), Space: 30.9 MB (12.27%) - LeetHub

--- 0962-flip-string-to-monotone-increasing/0962-flip-string-to-monotone-increasing.py
+++ 0962-flip-string-to-monotone-increasing/0962-flip-string-to-monotone-increasing.py
@@ -1,19 +1,8 @@
 class Solution:
     def minFlipsMonoIncr(self, s: str) -> int:
-        memo = {}        
-        def dp(i, lastChar):
-            if (i, lastChar) in memo: return memo[(i, lastChar)]
-
-            if i >= len(s): 
-                return 0
-
-            if lastChar == 1:
-                ans = (s[i] == '0') + dp(i + 1, 1)
-            else:
-                ans = min((s[i] == '1') + dp(i + 1, 0), (s[i] == '0') + dp(i + 1, 1))
-            
-            memo[(i, lastChar)] = ans
-            
-            return ans
+        dp = [[0, 0] for _ in range(len(s) + 1)]
+        for i in range(len(s) - 1, -1, -1):
+            dp[i][0] = min(dp[i + 1][0] + (s[i] == '1'), dp[i + 1][1] + (s[i] == '0'))
+            dp[i][1] = dp[i + 1][1] + (s[i] == '0')
         
-        return dp(0, 0)
+        return dp[0][0]
