dc0d7e3f95fa70597589ee1c2c4017796b5e37e0
Mathew Hany <mathew.hanybb@gmail.com>
2023-07-29 12:10:27 +0200

    Time: 6290 ms (50.26%), Space: 56 MB (83.15%) - LeetHub

--- 1393-maximum-value-of-k-coins-from-piles/1393-maximum-value-of-k-coins-from-piles.py
+++ 1393-maximum-value-of-k-coins-from-piles/1393-maximum-value-of-k-coins-from-piles.py
@@ -1,23 +1,15 @@
 class Solution:
     def maxValueOfCoins(self, piles: List[List[int]], k: int) -> int:
-        memo = {}
+        dp = [[0 for _ in range(k + 1)] for _ in range(len(piles) + 1)]
 
-        def dp(i, needed):
-            if (i, needed) in memo: return memo[(i, needed)]
+        for i in range(len(piles) - 1, -1, -1):
+            for needed in range(1, k + 1):
+                ans = dp[i + 1][needed]
+                currentSum = 0
+                for taken in range(needed):
+                    if taken >= len(piles[i]): break
+                    currentSum += piles[i][taken]
+                    ans = max(ans, currentSum + dp[i + 1][needed - taken - 1])
+                dp[i][needed] = ans
 
-            if needed == 0 or i >= len(piles): return 0
-
-            ans = dp(i + 1, needed)
-            currentSum = 0
-
-            for taken in range(needed):
-                if taken >= len(piles[i]): break
-
-                currentSum += piles[i][taken]
-                ans = max(ans, currentSum + dp(i + 1, needed - (taken + 1)))
-
-            memo[(i, needed)] = ans
-            
-            return ans
-        
-        return dp(0, k)
+        return dp[0][k]
