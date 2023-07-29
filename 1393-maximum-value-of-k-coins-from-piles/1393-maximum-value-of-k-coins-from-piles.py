345a3858ecee125086fdc0b856e897e432dac21e
Mathew Hany <mathew.hanybb@gmail.com>
2023-07-29 12:12:03 +0200

    Time: 5694 ms (70.32%), Space: 16.8 MB (98.13%) - LeetHub

--- 1393-maximum-value-of-k-coins-from-piles/1393-maximum-value-of-k-coins-from-piles.py
+++ 1393-maximum-value-of-k-coins-from-piles/1393-maximum-value-of-k-coins-from-piles.py
@@ -1,15 +1,15 @@
 class Solution:
     def maxValueOfCoins(self, piles: List[List[int]], k: int) -> int:
-        dp = [[0 for _ in range(k + 1)] for _ in range(len(piles) + 1)]
+        dp = [0 for _ in range(k + 1)]
 
         for i in range(len(piles) - 1, -1, -1):
-            for needed in range(1, k + 1):
-                ans = dp[i + 1][needed]
+            for needed in range(k, -1, -1):
+                ans = dp[needed]
                 currentSum = 0
                 for taken in range(needed):
                     if taken >= len(piles[i]): break
                     currentSum += piles[i][taken]
-                    ans = max(ans, currentSum + dp[i + 1][needed - taken - 1])
-                dp[i][needed] = ans
+                    ans = max(ans, currentSum + dp[needed - taken - 1])
+                dp[needed] = ans
 
-        return dp[0][k]
+        return dp[k]
