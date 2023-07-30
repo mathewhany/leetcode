495274da47fb86ec34c230c9c708725f157f544d
Mathew Hany <mathew.hanybb@gmail.com>
2023-07-30 09:12:01 +0200

    Time: 583 ms (95.29%), Space: 16.6 MB (98.68%) - LeetHub

--- 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.py
+++ 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.py
@@ -1,21 +1,20 @@
 class Solution:
     def minCost(self, n: int, cuts: List[int]) -> int:
+        cuts.append(0)
+        cuts.append(n)
         cuts.sort()
-        memo = {}
 
-        def dp(start, end):
-            if (start, end) in memo: return memo[(start, end)]
-
-            ans = inf
-            for cut in cuts[bisect_right(cuts, start):]:
-                if cut >= end: break
-                
-                ans = min(ans, (end - start) + dp(start, cut) + dp(cut, end))
-            
-            if ans == inf: ans = 0
-
-            memo[(start, end)] = ans
-            
-            return ans
+        dp = [[inf for _ in range(len(cuts))] for _ in range(len(cuts))]
         
-        return dp(0, n)
+        for i in range(len(cuts) - 1, -1, -1):
+            start = cuts[i]
+            for j in range(i + 1, len(cuts)):
+                end = cuts[j]
+                for cut in range(i + 1, j):
+                    dp[i][j] = min(
+                        dp[i][j],
+                        (end - start) + dp[i][cut] + dp[cut][j]
+                    )
+                dp[i][j] = dp[i][j] if dp[i][j] != inf else 0
+
+        return dp[0][len(cuts) - 1]
