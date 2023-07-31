0b881208e7a45fa2acb2e31dc10b63d749a32df2
Mathew Hany <mathew.hanybb@gmail.com>
2023-07-31 11:04:08 +0200

    Time: 124 ms (96.52%), Space: 17.3 MB (79.6%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,16 +1,22 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        queue = deque([(0, nums[0], 0)])
+        start, end = 0, 1
+        nextStart, nextEnd = 1, 1
+        jumps = 0
 
-        while queue:
-            current, jumpLen, jumps = queue.popleft()
-
-            if current == len(nums) - 1:
-                return jumps
+        for i in range(len(nums)):
+            if i >= end:
+                start, end = nextStart, nextEnd
+                nextStart, nextEnd = end + 1,  end + 1
+                jumps += 1
             
-            for j in range(current + 1, min(len(nums), current + jumpLen + 1)):
-                if nums[j] >= 0:
-                    queue.append((j, nums[j], jumps + 1))
-                    nums[j] = -1
+            if i < end:
+                nextEnd = max(nextEnd, i + nums[i] + 1)
+            else:
+                break
+        
+        if nextEnd > len(nums) - 1:
+            return jumps
         
         return -1
+            
