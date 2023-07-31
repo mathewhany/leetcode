f50b1c5b1911c6a74200c4da8a30710881102ee1
Mathew Hany <mathew.hanybb@gmail.com>
2023-07-31 09:48:36 +0200

    Time: 467 ms (84.09%), Space: 20.8 MB (77.85%) - LeetHub

--- 1020-longest-turbulent-subarray/1020-longest-turbulent-subarray.py
+++ 1020-longest-turbulent-subarray/1020-longest-turbulent-subarray.py
@@ -3,14 +3,14 @@ class Solution:
         best = 1
         dp = [0 for _ in range(len(arr))]
 
-        for i in range(len(arr) - 1, -1, -1):
-            if i == len(arr) - 1:
+        for i in range(len(arr)):
+            if i == 0:
                 dp[i] = (1, 1)
             else:
-                nextInc, nextDec = dp[i + 1]
+                prevInc, prevDec = dp[i - 1]
                 dp[i] = (
-                    1 + nextDec if arr[i] < arr[i + 1] else 1,
-                    1 + nextInc if arr[i] > arr[i + 1] else 1,
+                    1 + prevDec if arr[i] > arr[i - 1] else 1,
+                    1 + prevInc if arr[i] < arr[i - 1] else 1,
                 )
 
                 best = max(best, max(dp[i]))
