f4d6afbd6c72cb175a69cd81029ef52dbcab9ea8
Mathew Hany <mathew.hanybb@gmail.com>
2023-08-02 17:54:30 +0200

    Time: 44 ms (95.53%), Space: 16.4 MB (62.92%) - LeetHub

--- 1095-two-city-scheduling/1095-two-city-scheduling.py
+++ 1095-two-city-scheduling/1095-two-city-scheduling.py
@@ -1,17 +1,3 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        n = len(costs) // 2
-        dp = [[[inf for _ in range(n + 2)] for _ in range(n + 2)] for _ in range(2 * n + 1)]
-
-        for i in range(2 * n, -1, -1):
-            for aCount in range(n, -1, -1):
-                for bCount in range(n, -1, -1):
-                    if i >= 2 * n:
-                        dp[i][aCount][bCount] = 0
-                    else:
-                        dp[i][aCount][bCount] = min(
-                            costs[i][0] + dp[i + 1][aCount + 1][bCount], 
-                            costs[i][1] + dp[i + 1][aCount][bCount + 1]
-                        )
-
-        return dp[0][0][0]
+        return sum(aCost for aCost, _ in costs) + sum(sorted([bCost - aCost for aCost, bCost in costs])[:len(costs) // 2])
