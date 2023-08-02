271db86106c7677bd87f772e2123f3bfdf12919d
Mathew Hany <mathew.hanybb@gmail.com>
2023-08-02 17:29:17 +0200

    Time: 1114 ms (5.22%), Space: 26.7 MB (5.39%) - LeetHub

--- 1095-two-city-scheduling/1095-two-city-scheduling.py
+++ 1095-two-city-scheduling/1095-two-city-scheduling.py
@@ -1,20 +1,17 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
         n = len(costs) // 2
-        memo = {}
+        dp = [[[inf for _ in range(n + 2)] for _ in range(n + 2)] for _ in range(2 * n + 1)]
 
-        def dp(i, aCount, bCount):
-            if (i, aCount, bCount) in memo: return memo[(i, aCount, bCount)]
-            if aCount > n or bCount > n: return inf
+        for i in range(2 * n, -1, -1):
+            for aCount in range(n, -1, -1):
+                for bCount in range(n, -1, -1):
+                    if i >= 2 * n:
+                        dp[i][aCount][bCount] = 0
+                    else:
+                        dp[i][aCount][bCount] = min(
+                            costs[i][0] + dp[i + 1][aCount + 1][bCount], 
+                            costs[i][1] + dp[i + 1][aCount][bCount + 1]
+                        )
 
-            if i >= 2 * n: 
-                return 0
-
-            cost1 = costs[i][0] + dp(i + 1, aCount + 1, bCount)
-            cost2 = costs[i][1] + dp(i + 1, aCount, bCount + 1)
-
-            memo[(i, aCount, bCount)] = ans = min(cost1, cost2)
-
-            return ans
-        
-        return dp(0, 0, 0)
+        return dp[0][0][0]
