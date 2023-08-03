e5bf630ae769b62cc8dc9ad32a7c1c62714d04bf
Mathew Hany <mathew.hanybb@gmail.com>
2023-08-03 09:16:07 +0200

    Time: 39 ms (96.39%), Space: 16.8 MB (13.22%) - LeetHub

--- 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.py
+++ 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.py
@@ -1,29 +1,35 @@
+from sortedcontainers import SortedDict
+
 class SummaryRanges:
 
     def __init__(self):
-        self.heap = []
+        self.map = SortedDict()
 
     def addNum(self, value: int) -> None:
-        heappush(self.heap, value)
+        if value in self.map: return
+
+        right = self.map.bisect_left(value)
+        left = right - 1
+
+        keyRight = self.map.peekitem(right)[0] if right < len(self.map) else -1
+        keyLeft = self.map.peekitem(left)[0] if left >= 0 else -1
+        
+        if (keyRight != -1 and keyLeft != -1 and self.map[keyLeft] + 1 == value and value + 1 == keyRight):
+            self.map[keyLeft] = self.map[keyRight]
+            self.map.pop(keyRight)
+        elif (right != -1 and keyRight == value + 1):
+            self.map[value] = self.map.pop(keyRight)
+        elif (left != -1 and self.map[keyLeft] + 1 >= value):
+            self.map[keyLeft] = max(self.map[keyLeft], value)
+        else:
+            self.map[value] = value
 
     def getIntervals(self) -> List[List[int]]:
-        numbers = []
-        while self.heap: numbers.append(heappop(self.heap))
-
-        ans = []
-
-        prev = -inf
-        last = -inf
-
-        for n in numbers:
-            if not ans or ans[-1][1] + 1 < n:
-                ans.append([n, n])
-            else:
-                ans[-1][1] = n
-
-            heappush(self.heap, n)
-
+        ans = []        
+        for start in self.map:
+            ans.append([start, self.map[start]])
         return ans
+        
 
 # Your SummaryRanges object will be instantiated and called as such:
 # obj = SummaryRanges()
