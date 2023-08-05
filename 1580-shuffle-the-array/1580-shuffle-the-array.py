bf18429772f0683dde31d9e94df1f252fcf8b5f5
Mathew Hany <mathew.hanybb@gmail.com>
2023-08-05 08:32:24 +0200

    Time: 57 ms (97.25%), Space: 16.5 MB (70.92%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.py
+++ 1580-shuffle-the-array/1580-shuffle-the-array.py
@@ -1,9 +1,15 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        ans = []
-
         for i in range(n):
-            ans.append(nums[i])
-            ans.append(nums[n + i])
+            nums[i] = nums[i] << 10 | nums[n + i]
         
-        return ans
+        end = 2 * n - 1
+        for i in range(n - 1, -1, -1):
+            x = nums[i] >> 10
+            y = nums[i] & 0b1111111111
+
+            nums[end] = y
+            nums[end - 1] = x 
+            end -= 2
+        
+        return nums
