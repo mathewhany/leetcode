e28afb4ad37caed061d424538fe85bb4da1aecb1
Mathew Hany <mathew.hanybb@gmail.com>
2023-08-31 09:20:28 +0300

    Time: 402 ms (56.59%), Space: 56.7 MB (43.4%) - LeetHub

--- 1794-minimize-deviation-in-array/1794-minimize-deviation-in-array.cpp
+++ 1794-minimize-deviation-in-array/1794-minimize-deviation-in-array.cpp
@@ -2,36 +2,22 @@ class Solution {
 public:
     int minimumDeviation(vector<int>& nums) {
         int n = nums.size();
-        int maxVal = INT_MIN;
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> queue;
+        int minVal = INT_MAX;
+        priority_queue<int> pq;
         
         for (int i = 0; i < n; i++) {
-            int value = nums[i];
-            int limit = nums[i] % 2 == 0 ? nums[i] : nums[i] * 2;
-
-            while (value % 2 == 0) {
-                value /= 2;
-            }
-
-            maxVal = max(maxVal, value);
-            queue.push(make_pair(value, limit));
+            int value = nums[i] % 2 == 1 ? nums[i] * 2 : nums[i];
+            minVal = min(minVal, value);
+            pq.push(value);
         }
 
-        int ans = INT_MAX;
-
-        while (true) {
-            auto [minVal, minLimit] = queue.top();
-            
-            ans = min(ans, maxVal - minVal);
-
-            if (minVal * 2 > minLimit) {
-                break;
-            } else if (minVal * 2 > maxVal) {
-                maxVal = minVal * 2;
-            }
-
-            queue.pop();
-            queue.push(make_pair(minVal * 2, minLimit));
+        int ans = pq.top() - minVal;
+        while (pq.top() % 2 == 0) {
+            int val = pq.top() / 2;
+            minVal = min(minVal, val); 
+            pq.pop();
+            pq.push(val);
+            ans = min(ans, pq.top() - minVal);
         }
 
         return ans;
