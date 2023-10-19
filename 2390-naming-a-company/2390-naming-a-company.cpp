4276045c7210e2fad91587a47d2b8986082f656c
Mathew Hany <mathew.hanybb@gmail.com>
2023-10-19 08:46:08 +0300

    Time: 706 ms (52.72%), Space: 83.8 MB (68.2%) - LeetHub

--- 2390-naming-a-company/2390-naming-a-company.cpp
+++ 2390-naming-a-company/2390-naming-a-company.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
     long long distinctNames(vector<string>& ideas) {
-        unordered_set<string> prefix_to_suffixes[26];
+        vector<unordered_set<string>> groups(26);
 
-        for (auto const &idea : ideas) {
-            int prefix = idea[0] - 'a';
-            string suffix = idea.substr(1);
-        
-            prefix_to_suffixes[prefix].insert(suffix);
+        for (int i = 0; i < ideas.size(); i++) {
+            char prefix = ideas[i][0];
+            string suffix = ideas[i].substr(1);
+            
+            groups[prefix - 'a'].insert(suffix);
         }
 
         long ans = 0;
-
         for (int i = 0; i < 26; i++) {
             for (int j = i + 1; j < 26; j++) {
-                long c1 = 0;
-                long c2 = 0;
+                long count1 = 0, count2 = 0;
 
-                for (auto const& suffix : prefix_to_suffixes[i]) {
-                    if (!prefix_to_suffixes[j].count(suffix)) c1++;
+                for (auto const suffix : groups[i]) {
+                    if (groups[j].find(suffix) == groups[j].end()) {
+                        count1++;
+                    }
                 }
-                for (auto const& suffix : prefix_to_suffixes[j]) {
-                    if (!prefix_to_suffixes[i].count(suffix)) c2++;
+
+                for (auto const suffix : groups[j]) {
+                    if (groups[i].find(suffix) == groups[i].end()) {
+                        count2++;
+                    }
                 }
-                ans += c1 * c2;
+
+                ans += count1 * count2;
             }
         }
 
