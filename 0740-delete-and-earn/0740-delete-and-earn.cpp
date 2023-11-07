6f1130610814c4bf2c96c4d1c5e9ef610feb9971
Mathew Hany <mathew.hanybb@gmail.com>
2023-11-07 09:20:40 +0200

    Time: 7 ms (85.41%), Space: 12.5 MB (79.38%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -1,33 +1,19 @@
 class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
-        map<int, int> numToFreq;
-        for (int i = 0; i < nums.size(); i++) numToFreq[nums[i]]++;
+        vector<long> freq(*max_element(nums.begin(), nums.end()) + 1);
+        for (int i = 0; i < nums.size(); i++) {
+            freq[nums[i]]++;
+        }
         
-        vector<long> freqs;
-        vector<long> uniqueNums;
-        int n = numToFreq.size();
-        for (auto const &[num, freq] : numToFreq) {
-            freqs.push_back(freq);
-            uniqueNums.push_back(num);
+        vector<long> dp(freq.size());
+        for (int i = 0; i < freq.size(); i++) {
+            dp[i] = max(
+                (i >= 2 ? dp[i -  2] : 0) + freq[i] * i,
+                (i >= 1) ? dp[i - 1] : 0
+            );
         }
 
-        vector<long> dp(n + 1, 0);
-
-        for (int i = n - 1; i >= 0; i--) {
-            long num  = uniqueNums[i];
-            long freq = freqs[i];
-
-            if (i == n - 1) {
-                dp[i] = num * freq;
-            } else {
-                dp[i] = max(
-                    dp[i + 1],
-                    num * freq + dp[num + 1 == uniqueNums[i + 1] ? i + 2 : i + 1]
-                );
-            }
-        }
-
-        return dp[0];
+        return dp[freq.size() - 1];
     }
 };
