10e562a3b021fd9b7544e02c1ab7484117a3bb6c
Mathew Hany <mathew.hanybb@gmail.com>
2023-11-08 09:12:30 +0200

    Time: 231 ms (27.02%), Space: 123.7 MB (12.21%) - LeetHub

--- 2096-find-the-longest-valid-obstacle-course-at-each-position/2096-find-the-longest-valid-obstacle-course-at-each-position.cpp
+++ 2096-find-the-longest-valid-obstacle-course-at-each-position/2096-find-the-longest-valid-obstacle-course-at-each-position.cpp
@@ -1,19 +1,31 @@
 class Solution {
 public:
     vector<int> longestObstacleCourseAtEachPosition(vector<int>& obstacles) {
-        vector<int> dp, ans;
+        vector<int> dp;
+        vector<int> lengths;
 
         for (int i = 0; i < obstacles.size(); i++) {
-            if (dp.empty() || dp[dp.size() - 1] <= obstacles[i]) {
+            int lo = 0;
+            int hi = dp.size() - 1;
+            int ans = -1;
+            while (lo <= hi) {
+                int mid = lo + (hi - lo) / 2;
+                if (dp[mid] > obstacles[i]) {
+                    hi = mid - 1;
+                    ans = mid;
+                } else {
+                    lo = mid + 1;
+                }
+            }
+            if (ans == -1) {
                 dp.push_back(obstacles[i]);
-                ans.push_back(dp.size());
+                lengths.push_back(dp.size());
             } else {
-                int idx = upper_bound(dp.begin(), dp.end(), obstacles[i]) - dp.begin();
-                ans.push_back(idx + 1);
-                dp[idx] = obstacles[i];
+                dp[ans] = obstacles[i];
+                lengths.push_back(ans + 1);
             }
         }
 
-        return ans;
+        return lengths;
     }
 };
