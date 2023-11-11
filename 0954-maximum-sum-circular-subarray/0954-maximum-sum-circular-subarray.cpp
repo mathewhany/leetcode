ff2988c0c7eba7f41b7254386ac47c983b7e03f3
Mathew Hany <mathew.hanybb@gmail.com>
2023-11-11 08:06:53 +0200

    Time: 40 ms (94.23%), Space: 40.2 MB (39.27%) - LeetHub

--- 0954-maximum-sum-circular-subarray/0954-maximum-sum-circular-subarray.cpp
+++ 0954-maximum-sum-circular-subarray/0954-maximum-sum-circular-subarray.cpp
@@ -1,35 +1,20 @@
 class Solution {
 public:
     int maxSubarraySumCircular(vector<int>& nums) {
-        int n = nums.size();
-        vector<long> prefix, dp;
-        long prefixSum = 0, suffixSum = 0;
-        long dpBest = INT_MIN;
+        long sumLargest = 0, sumSmallest = 0, total = 0, smallest = LONG_MAX, largest = LONG_MIN;
 
-        for (int i = 0; i < n; i++) { 
-            prefixSum += nums[i];
-
-            if (i == 0) {
-                prefix.push_back(prefixSum);
-                dp.push_back(nums[i]);
-            } else {
-                prefix.push_back(
-                    max(prefix[i - 1], prefixSum)
-                );
-                dp.push_back(max(dp[i - 1] + nums[i], long(nums[i])));
-            }
-
-            dpBest = max(dpBest, dp[i]);
+        for (int i = 0; i < nums.size(); i++) {
+            long n = nums[i];
+            sumLargest = max(n, sumLargest + n);
+            largest = max(sumLargest, largest);
+            sumSmallest = min(n, sumSmallest + n);
+            smallest = min(sumSmallest, smallest);
+            total += n;
         }
 
-        long suffix = INT_MIN;
-        long best = dpBest;
-        for (int i = 0; i < n; i++) {
-            best = max(best, prefix[nums.size() - 1 - i] + suffix);
-            suffixSum += nums[nums.size() - 1 - i];
-            suffix = max(suffix, suffixSum);
-        }
-
-        return best;
+        return max(
+            largest,
+            total != smallest ? total - smallest : LONG_MIN
+        );
     }
 };
